129208bc00b69a6c9530612591b21717d074ae3b
William Homer <billyhomeriii@gmail.com>
2019-03-21 11:53:24 -0600

    Add the methods for Game skeleton

--- lib/game.rb
+++ lib/game.rb
@@ -6,7 +6,9 @@ class Game
 
   def start
     main_menu
-    puts "game has started!!!"
+    setup_game
+    start_turns
+    end_game
   end
 
   def main_menu
@@ -19,6 +21,38 @@ class Game
       puts "Invalid input!"
       main_menu
     end
+  end
+
+  def setup_game
+    # This method will call the helper methods below to setup the game.
+    setup_computer_place_ships
+    setup_player_place_ships
+  end
+
+  def setup_computer_place_ships
+    # this method will execute the code for the computer to place ships.
+    puts "Computer is placing ships!"
+    sleep(1.5); print " .. "
+    sleep(1); print " ... "
+    sleep(1.5); print " .. "
+  end
+
+  def setup_player_place_ships
+    #this method will execute the code for the player to place ships.
+    puts "\nplayer place your ships."
+  end
+
+  def start_turns
+    turns_display_boards
+    turns_player_shot
+    turns_computer_shot
+    turns_results
+  end
+
+  def end_game
+    #this method will end the game
+    "You won!"
+    "I won!"
   end
 
 end
