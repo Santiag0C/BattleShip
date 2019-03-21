050d574a0d2d6ba3dc0ef552ac063c4227307265
William Homer <billyhomeriii@gmail.com>
2019-03-21 11:01:55 -0600

    Added main_menu method which presents and asks user if they want to start a new game

--- lib/game.rb
+++ lib/game.rb
@@ -4,4 +4,21 @@ require './lib/board'
 
 class Game
 
+  def start
+    main_menu
+    puts "game has started!!!"
+  end
+
+  def main_menu
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    input = gets.chomp
+    if input.upcase == "Q"
+      exit
+    elsif input.upcase != "P"
+      puts "Invalid input!"
+      main_menu
+    end
+  end
+
 end
